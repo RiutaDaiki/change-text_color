075f2955c7aed0749d4c821e91a5cf9b9e5187b4
riuta <daikiriuta0926@gmail.com>
2020-12-18 17:39:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/button_practice/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.button_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.time.chrono.JapaneseEra.values
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        redBtn.setOnClickListener {
+            colorText.textColors
+        }
+
+    }
+
+
+}
